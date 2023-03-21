d8a23a2859920cf5caca6debd33dd3c2023c3494
JeongJiSang <jsj00203@gmail.com>
2023-03-21 16:14:44 +0900

    로그인 기능 초안

--- src/main/java/com/chatting/server/model/main/ServerReceiver.java
+++ src/main/java/com/chatting/server/model/main/ServerReceiver.java
@@ -2,12 +2,14 @@ package com.chatting.server.model.main;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import com.chatting.server.model.Protocol;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
@@ -50,35 +52,59 @@ public class ServerReceiver extends Thread{
             run_start://while문같은 반복문 전체를 빠져 나가도록 처리할 때
             while (!isStop) {
 
-            	logger.info("메세지를 수신했습니다.");
+                String msg = ois.readObject().toString(); //클라이언트로 부터 오는 메세지 수신 담당, 항상 메세지를 받은 이후부터 모든 서버업무가 수행이 가능함
+				logger.info("{} - 메세지를 수신했습니다.", msg);
 
-                String msg = ois.readObject().toString();
-                logger.info(msg);
-                
-    
+				String[] loginResult = msg.split(" ");
 
-                String s = "";
+				switch (loginResult[0]){ //첫번째 코드로 메세지 실행 내용 분류
+					case Protocol.checkLogin:
 
-                
-                if ("회원가입 버튼 클릭".equals(msg)) {
-                    s = "환영합니다.";
-                    oos.writeObject(s);
-                } 
-                
-                
-                if ("로그인 버튼 클릭".equals(msg)) {
-                	s = "로그인 되었습니다.";
-                	oos.writeObject(s);
-                }
+						String id = loginResult[1];
+						String pw = loginResult[2];
 
-                if("테스트 진행".equals(msg)){
-                    s = "테스트 대응합니다.";
-                    oos.writeObject(s);
-                }
-                
-               
+						URL resource = getClass().getClassLoader().getResource("user-info.xml");
+						Document document = new SAXBuilder().build(resource);
 
-            }
+						Element root = document.getRootElement();
+						List<Element> userList = root.getChildren("user");
+
+						List<Map<String, String>> result = new ArrayList<>();
+
+						for (Element element : userList) {
+							Map<String, String> user = new HashMap<>();
+							user.put("id", element.getChildText("id"));
+							user.put("pw", element.getChildText("pw"));
+							user.put("name", element.getChildText("name"));
+
+							result.add(user);
+
+						}
+
+						if(userList.contains(pw)) {
+							oos.writeObject("로그인 성공");
+						} else {
+							oos.writeObject("로그인 실패");
+						}
+
+
+
+
+
+
+						break;
+					case Protocol.createRoomView:
+
+
+						break;
+					default:
+
+						oos.writeObject("테스트 완료");
+						break;
+				}
+
+
+			}
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
@@ -86,85 +112,17 @@ public class ServerReceiver extends Thread{
         }
     }
     
-    public void checkLogin(List<Map<String, String>> userList) {
-    	
-    	
-		try {
-			String msg = ois.readObject().toString();
-			logger.info(msg);
-			
-			String[] loginResult = msg.split(" ");
-			String result = loginResult[0];
-			
-			if("100".equals(result)) {
-				parseCheck();
+	public boolean validateUser(String id, String pw){
+		List<Map<String, String>> userList = getUserList();
 
-				String id = loginResult[1];
-				String pw = loginResult[2];
-		    	
-				if(userList.contains(id)) {
-					if(userList.contains(pw)) {
-						oos.writeObject("로그인 성공");						
-					} else {
-						oos.writeObject("로그인 실패");		
-					}
-				} else {
-					oos.writeObject("로그인 실패");		
-				}
-				
-            } 
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	
-    	
-    }
-    
-    
-    public List<Map<String, String>> parseCheck() {
-    	String filePath = "E:\\project_jklee\\jjserver\\src\\main\\resources\\user-info.xml";
-    	
-    	File file = new File(filePath);
-    	
-    	SAXBuilder saxBuilder = new SAXBuilder();
-    	
-		try {
-			Document doc = saxBuilder.build(file);
-			Element root = doc.getRootElement();
-			
-			List<Element> userList = root.getChildren("users");
 
-			
-			List<Map<String, String>> result = new ArrayList<Map<String,String>>();
-			
-			for(Iterator<Element> iter = userList.iterator(); iter.hasNext();) {
-				Element element = iter.next();
-				
-				Map<String, String> user = new HashMap<String, String>();
-				user.put("id", element.getChildText("id"));
-				user.put("pw", element.getChildText("pw"));
-				user.put("name", element.getChildText("name"));
-				
-				result.add(user);
-			}
-			
-			return result;
-			
-		} catch (JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
-		
-		
-    }
+		return true;
+	}
+
+	public List<Map<String, String>> getUserList(){
+		List<Map<String, String>> list = new ArrayList<>();
+
+
+		return list;
+	}
 }
